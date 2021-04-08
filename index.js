07a3e10ac6afb993002614d2ab1b41fb7748f6ec
sahar-fehri <fehrisahar14@gmail.com>
2021-04-08 20:49:36 +0000

    fix

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const {
 
 
 
-
 //Importing routes
 const authRoute = require('./routes/auth');
 const roomRoute = require('./routes/room');
@@ -32,37 +31,16 @@ app.use('/api/room', roomRoute);
 
 
 (async () => {
-  /*  let start = 1617651955152;
-    let end = start + 1 * 3600;
-    let startHex = web3.utils.asciiToHex(start.toString()).padEnd(66, "0");
-    let endHex = web3.utils.asciiToHex(end.toString()).padEnd(66, "0");
-    const idSlot = new BN('1');
-    const MAP_COLA = COLA.map(elm => web3.utils.asciiToHex(elm).padEnd(66, "0"));
-    const MAP_PEPSI = PEPSI.map(elm => web3.utils.asciiToHex(elm).padEnd(66, "0"));
-    const ID_COLA = web3.utils.asciiToHex("COLA").padEnd(66, "0")
-    const ID_PEPSI = web3.utils.asciiToHex("PEPSI").padEnd(66, "0")
-    let accounts = await web3.eth.getAccounts()
-    
-   */
-
     try{
         let instance = await deployBookingContract();
        
         for(let event of EVENT_NAMES){
             subscribeLogEvent(instance, event);
         }
-
-      //  let receipt = await instance.book(ID_COLA, MAP_COLA[1], startHex, endHex, idSlot, {from: accounts[0], gas: "220000"})
-       // console.log(receipt)
-
-
     }catch(error){
         console.log(error)
     }
 })();
-
-
-
 
 
 app.listen(3000, ()=> {
